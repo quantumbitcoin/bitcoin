bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

QA: Functional test for sweepprivkeys

test/functional/test_runner.py
@@ -82,6 +82,7 @@ BASE_SCRIPTS= [
     'keypool-topup.py',
     'zmq_test.py',
     'mempool_resurrect_test.py',
+    'sweepprivkeys.py',
     'txn_doublespend.py --mineblock',
     'txn_clone.py',
     'getchaintips.py',

quantumbitcoin/bitcoin

test/functional/test_runner.py
@@ -88,6 +88,7 @@ BASE_SCRIPTS = [
     'interface_zmq.py',
     'interface_bitcoin_cli.py',
     'mempool_resurrect.py',
+    'wallet_sweepprivkeys.py',
     'wallet_txn_doublespend.py --mineblock',
     'wallet_txn_clone.py',
     'wallet_txn_clone.py --segwit',
bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

ArgsManager: Queue early ModifyRWConfigFile changes until rwconf file is loaded

src/util.cpp
@@ -650,6 +650,10 @@ void ArgsManager::ReadRWConfigFile(const std::string& confPath)
     std::unordered_set<std::string> rwconf_assigned;
     ReadConfigFile(streamRWConfig, &args_assigned_by_conf, &rwconf_assigned);
     args_assigned_by_conf.clear();
+    if (!rwconf_queued_writes.empty()) {
+        ModifyRWConfigFile(rwconf_queued_writes);
+        rwconf_queued_writes.clear();
+    }
     rwconf_had_prune_option = rwconf_assigned.count("prune");
 }
 
@@ -815,7 +819,13 @@ void ModifyRWConfigFile(std::istream& streamIn, std::ostream& streamOut, const s
 
 void ArgsManager::ModifyRWConfigFile(const std::map<std::string, std::string>& mapChangeSettings)
 {
-    assert(!rwconf_path.empty());
+    if (rwconf_path.empty()) {
+        // Queue the change for after rwconf is loaded
+        for (auto& it : mapChangeSettings) {
+            rwconf_queued_writes[it.first] = it.second;
+        }
+        return;
+    }
     fs::path rwconf_new_path = rwconf_path;
     rwconf_new_path += ".new";
     const std::string new_path_str = rwconf_new_path.string();

quantumbitcoin/bitcoin

src/util.cpp
@@ -1009,6 +1009,10 @@ bool ArgsManager::ReadConfigFiles(std::string& error, bool ignore_invalid_keys)
             return false;
         }
     }
+    if (!rwconf_queued_writes.empty()) {
+        ModifyRWConfigFile(rwconf_queued_writes);
+        rwconf_queued_writes.clear();
+    }
 
     return true;
 }
@@ -1191,7 +1195,13 @@ void ModifyRWConfigStream(std::istream& stream_in, std::ostream& stream_out, con
 
 void ArgsManager::ModifyRWConfigFile(const std::map<std::string, std::string>& settings_to_change)
 {
-    assert(!rwconf_path.empty());
+    if (rwconf_path.empty()) {
+        // Queue the change for after rwconf is loaded
+        for (auto& it : settings_to_change) {
+            rwconf_queued_writes[it.first] = it.second;
+        }
+        return;
+    }
     fs::path rwconf_new_path = rwconf_path;
     rwconf_new_path += ".new";
     const std::string new_path_str = rwconf_new_path.string();
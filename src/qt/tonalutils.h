c91222502403d4fe6448b93876d72b0ca7d042b9
Luke Dashjr <luke-jr+git@utopios.org>
2018-12-14 15:47:30 +0000

    fixup! Support for Tonal Bitcoin units (ᵇTBC, ˢTBC, and TBC)

--- src/qt/tonalutils.h
+++ src/qt/tonalutils.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef TONALUTILS_H
-#define TONALUTILS_H
+#ifndef BITCOIN_QT_TONALUTILS_H
+#define BITCOIN_QT_TONALUTILS_H
 
 #include <QValidator>
 
@@ -22,4 +22,4 @@ public:
     static void ConvertToHex(QString&);
 };
 
-#endif
+#endif // BITCOIN_QT_TONALUTILS_H
